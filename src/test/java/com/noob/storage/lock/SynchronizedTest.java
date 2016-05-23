a5adfb4ed44f307b48d4fedae34bd8a63e2b4902
luyun <zhwwanwan@hotmail.com>
2016-05-23 10:08:59 +0800

    20160523

--- src/test/java/com/noob/storage/lock/SynchronizedTest.java
+++ src/test/java/com/noob/storage/lock/SynchronizedTest.java
@@ -2,22 +2,10 @@ package com.noob.storage.lock;
 
 public class SynchronizedTest {
 
-    private final Object o = new Object();
+    private int x = 0;
 
     public static void main(String[] args) {
 
-    }
-
-    public void SyncObject() throws InterruptedException {
-        synchronized (o){
-            o.wait();
-        }
-    }
-
-    public void notifyObject(){
-        synchronized (o){
-            o.notify();
-        }
     }
 
 }
